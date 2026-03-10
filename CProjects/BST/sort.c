700103951be59d0e45ac542baeb0bd20847ecf11
Greg <grzegorzkaczmarek2017@gmail.com>
2026-03-10 10:59:34 +0100

    BST, removeMin recursive

--- CProjects/BST/sort.c
+++ CProjects/BST/sort.c
@@ -31,7 +31,7 @@ void printAll(Tree t) {
     if (t == NULL)
         return;
     printAll(t->left);
-    printf("%d\n", t->value);
+    printf("Node_value: %d\n", t->value);
     printAll(t->right);
 }
 
@@ -41,6 +41,30 @@ void removeAll(Tree t) {
     removeAll(t->left);
     removeAll(t->right);
     free(t);
+}
+
+int removeMinRec(Tree *treePtr, Tree *parent) {
+    assert((*treePtr) != NULL);
+    Tree *left = &((*treePtr)->left);
+    if (*left != NULL) {
+        return removeMinRec(left, treePtr);
+    }
+    else {
+        int result = (*treePtr)->value;
+        Tree *right = &((*treePtr)->right);
+        if (*right != NULL && parent != NULL) {
+            (*parent)->left = *right;
+        }
+        else if (parent != NULL) {
+            (*parent) -> left = NULL;
+        }
+        free(*treePtr);
+        return result;
+    }
+}
+
+int removeMin(Tree *treePtr) {
+    return removeMinRec(treePtr, NULL);
 }
 
 
@@ -53,6 +77,7 @@ int main() {
         scanf("%d", &x);
         insert(&treePtr, x);
     }
+    printf("Deleted: %d\n",removeMin(&treePtr));
     printAll(treePtr);
     removeAll(treePtr);
     return 0;
